6945bc9dfebf0d516d351b46d90bc8b54aa3ad7d
Jody Chambers <jody.chambers@hootsuite.com>
2016-11-05 22:42:52 -0700

    My first commit that is broken

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
+	console.log("fart");
 	$.ajax({
 	    url: 'https://roaster3000.herokuapp.com/roast?email=jody.chambers@hootsuite.com',
 	    dataType: 'application/json',
+	    head : ok,
 	    success: function(response){ 
 	      console.log(response);
-	      $('.fart').(response);
+	      $('.fart').text(response);
+	  	},
+	  	error: function(error){
+	  		console.log(error);
+	  		$('.fart').text(response.responseText);
 	  	}
   	});
  });
