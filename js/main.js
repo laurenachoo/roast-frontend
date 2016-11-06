5a4b03121cc327dc91af46f2e2789e2624a0563a
Jody Chambers <jody.chambers@hootsuite.com>
2016-11-06 13:01:18 -0800

    My changees

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
-	console.log("fart");
-	$.ajax({
-	    url: 'https://roaster3000.herokuapp.com/roast?email=jody.chambers@hootsuite.com',
-	    dataType: 'application/json',
-	    head : ok,
-	    success: function(response){ 
-	      console.log(response);
-	      $('.fart').text(response);
-	  	},
-	  	error: function(error){
-	  		console.log(error);
-	  		$('.fart').text(response.responseText);
-	  	}
-  	});
+
+	// $('.theForm').on('submit', function(){
+		var email = $('.theEmail').val();
+	
+		console.log('Email: ', email);
+		$.getJSON('http://roaster3000.herokuapp.com/roast?email=jody.chambers@hootsuite.com')
+		  .done(function(data) {
+		    console.log('success')
+		    console.log(data);
+
+		    
+
+
+
+
+
+		  }).fail(function(jqXHR, status, err) {
+		    console.error('fuck')
+		  });
+	// })
  });
+
